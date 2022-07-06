5c2bfbe85b25b8248998ba772e901c68491f0fd9
Gabriel Tonhatti <gabrieltonhatti37@gmail.com>
2022-07-05 21:05:12 -0300

    Cors configuration

--- hr-api-gateway-zuul/src/main/java/com/devsuperior/hrapigatewayzuul/config/ResourceServerConfig.java
+++ hr-api-gateway-zuul/src/main/java/com/devsuperior/hrapigatewayzuul/config/ResourceServerConfig.java
@@ -1,6 +1,7 @@
 package com.devsuperior.hrapigatewayzuul.config;
 
 import java.util.Arrays;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
@@ -22,57 +23,57 @@ import org.springframework.web.filter.CorsFilter;
 @EnableResourceServer
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
-	@Autowired
-	private JwtTokenStore tokenStore;
+    @Autowired
+    private JwtTokenStore tokenStore;
 
-	private final static String[] PUBLIC = { "/hr-oauth/oauth/token" };
-	private final static String[] OPERATOR = { "/hr-worker/**" };
-	private final static String[] ADMIN = { 
-			"/hr-payroll/**",
-			"/hr-user/**",
-			"/actuador/**",
-			"hr-worker/actuador/**",
-			"hr-oauth/actuador/**" 
-	};
+    private final static String[] PUBLIC = {"/hr-oauth/oauth/token"};
+    private final static String[] OPERATOR = {"/hr-worker/**"};
+    private final static String[] ADMIN = {
+            "/hr-payroll/**",
+            "/hr-user/**",
+            "/actuador/**",
+            "hr-worker/actuador/**",
+            "hr-oauth/actuador/**"
+    };
 
-	@Override
-	public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
-		resources.tokenStore(tokenStore);
-	}
+    @Override
+    public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+        resources.tokenStore(tokenStore);
+    }
 
-	@Override
-	public void configure(HttpSecurity http) throws Exception {
-		http
-			.authorizeRequests()
-			.antMatchers(PUBLIC).permitAll()
-			.antMatchers(HttpMethod.GET, OPERATOR).hasAnyRole("OPERATOR", "ADMIN")
-			.antMatchers(ADMIN).hasRole("ADMIN")
-			.anyRequest().authenticated()
-			.and()
-			.cors()
-			.configurationSource(corsfigurationSource());
-	}
-	
-	@Bean
-	public CorsConfigurationSource corsfigurationSource() {
-		CorsConfiguration corsConfig = new CorsConfiguration();
-		corsConfig.setAllowedOrigins(Arrays.asList("*"));
-		corsConfig.setAllowedMethods(Arrays.asList("POST", "GET", "PUT", "DELETE", "PATCH"));
-		corsConfig.setAllowCredentials(true);
-		corsConfig.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
-		
-		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		source.registerCorsConfiguration("/**", corsConfig);
-		
-		return source;
-	}
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers(PUBLIC).permitAll()
+                .antMatchers(HttpMethod.GET, OPERATOR).hasAnyRole("OPERATOR", "ADMIN")
+                .antMatchers(ADMIN).hasRole("ADMIN")
+                .anyRequest().authenticated()
+                .and()
+                .cors()
+                .configurationSource(corsConfigurationSource());
+    }
 
-	@Bean
-	public FilterRegistrationBean<CorsFilter> corsFilter() {
-		FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(new CorsFilter(corsfigurationSource()));
-		bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
-		
-		return bean;
-	}
-	
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration corsConfig = new CorsConfiguration();
+        corsConfig.setAllowedOrigins(List.of("*"));
+        corsConfig.setAllowedMethods(Arrays.asList("POST", "GET", "PUT", "DELETE", "PATCH"));
+        corsConfig.setAllowCredentials(true);
+        corsConfig.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfig);
+
+        return source;
+    }
+
+    @Bean
+    public FilterRegistrationBean<CorsFilter> corsFilter() {
+        FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(new CorsFilter(corsConfigurationSource()));
+        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+
+        return bean;
+    }
+
 }
